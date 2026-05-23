609bf0d07902b6dc4677641808e364ccacf9d3a5
Bluesky0330 <solachicken@gmail.com>
2026-05-23 18:02:10 +0900

    ここからはじめる

--- DI_v0_0/src/_constant.hpp
+++ DI_v0_0/src/_constant.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _CONSTANT
 #define _CONSTANT
 
@@ -8,15 +7,4 @@
 #define MATH_TOLERANCE 1.0e-11
 #define MERGE_ERROR 1.0e-11
 
-=======
-#ifndef _CONSTANT
-#define _CONSTANT
-
-#define MAX_DIMENSION 3
-#define ERROR -999
-#define TOLERANCE 1.0e-11
-#define MATH_TOLERANCE 1.0e-11
-#define MERGE_ERROR 1.0e-11
-
->>>>>>> 2fb143b0c96cc7f27a32b9e8b589a1bba0444f55
 #endif
